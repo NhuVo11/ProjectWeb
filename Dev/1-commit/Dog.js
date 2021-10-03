617e2c705551ec61a01879bf53b7f1d95cd65903
NhuVo11 <nhuvo1408@gmail.com>
2021-10-03 10:36:09 +0700

    ADD modifier

--- Dev/1-commit/Dog.js
+++ Dev/1-commit/Dog.js
@@ -1,6 +1,7 @@
-function Dog(Color){
+function Dog(Color, Weight){
     this.stomach = [];
     this.Color = Color;
+    this.Weight = Weight; 
 }
 Dog.prototype.eat = function(cat){
     this.stomach.push(cat);
